cc03327e5fe6eded9c1ba5dc6e34acde92e94b59
andreleao1 <andre.gustavo.ls@hotmail.com>
2022-01-26 15:54:58 -0300

    first commit

---
+++ src/main/java/br/com/agls/TodoListApplication.java
@@ -0,0 +1,9 @@
+package br.com.agls;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class TodoListApplication extends Application{
+
+}
